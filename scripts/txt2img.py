5e9ca43311168dd4f942779505d99ae0d9dea45e
Raymonf <raymonf@outlook.com>
2022-08-21 00:58:28 -0400

    Make MPS support work (but ugly)
    Based on https://github.com/CompVis/stable-diffusion/pull/47

--- scripts/txt2img.py
+++ scripts/txt2img.py
@@ -16,6 +16,14 @@ from ldm.util import instantiate_from_config
 from ldm.models.diffusion.ddim import DDIMSampler
 from ldm.models.diffusion.plms import PLMSSampler
 
+def get_device():
+    if(torch.cuda.is_available()):
+        return 'cuda'
+    elif(torch.backends.mps.is_available()):
+        return 'mps'
+    else:
+        return 'cpu'
+
 
 def chunk(it, size):
     it = iter(it)
@@ -37,7 +45,7 @@ def load_model_from_config(config, ckpt, verbose=False):
         print("unexpected keys:")
         print(u)
 
-    model.cuda()
+    model.to(get_device())
     model.eval()
     return model
 
@@ -187,12 +195,7 @@ def main():
     config = OmegaConf.load(f"{opt.config}")
     model = load_model_from_config(config, f"{opt.ckpt}")
 
-    if(torch.cuda.is_available()):
-        device = torch.device("cuda")
-    elif(torch.backends.mps.is_available()):
-        device = torch.device("mps")
-    else:
-        device = torch.device("cpu")
+    device = torch.device(get_device())
     model = model.to(device)
 
     if opt.plms:
@@ -226,8 +229,10 @@ def main():
         start_code = torch.randn([opt.n_samples, opt.C, opt.H // opt.f, opt.W // opt.f], device=device)
 
     precision_scope = autocast if opt.precision=="autocast" else nullcontext
+    if device.type == 'mps':
+        precision_scope = nullcontext # have to use f32 on mps
     with torch.no_grad():
-        with precision_scope("cuda"):
+        with precision_scope(device.type):
             with model.ema_scope():
                 tic = time.time()
                 all_samples = list()
